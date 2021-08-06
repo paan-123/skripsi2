74f32869224268e7b70a2e79d441837ea9d7b55d
unknown <farhanmozart@mail.com>
2021-08-07 00:45:07 +0700

    6

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,21 +36,13 @@
 			<div class="wrap-login100">
 				<form class="login100-form validate-form">
 					<span class="login100-form-title p-b-34">
-						Account Login
+						 SIMAS HIDAYATUL FALAH
 					</span>
 					
-					<div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
-						<input id="first-name" class="input100" type="text" name="username" placeholder="User name">
-						<span class="focus-input100"></span>
-					</div>
-					<div class="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
-						<span class="focus-input100"></span>
-					</div>
 					
 					<div class="container-login100-form-btn">
                         {{-- <a href="{{route('google.login')}}" class="btn btn-block btn-danger">Login</a> --}}
-                        <a href="{{route ('google.login')}}" class="btn btn-block btn-danger">Login</a>
+                        <a href="{{route ('google.login')}}" class="btn btn-block btn-danger"> <i class="fa fa-google-plus"></i> LOGIN</a>
 						{{-- <button class="login100-form-btn">
 							Sign in
 						</button> --}}
