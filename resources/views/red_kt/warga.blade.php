4ae2c91d59d5da8d96afb5b5bc29cb8363c38279
bagas5ri <bagastitik@gmail.com>
2021-08-12 04:07:24 +0700

    cFilter for Karangtaruna

--- resources/views/red_kt/warga.blade.php
+++ resources/views/red_kt/warga.blade.php
@@ -53,33 +53,58 @@
                                         <select class="select form-control"></select>
                                     </div>
                                 </div>
+                                <div class="col-sm-3" hidden>
+                                    <div class="form-group">
+                                        <label class="control-label">RW</label>
+                                        <select class="select form-control"></select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3" hidden>
+                                    <div class="form-group">
+                                        <label class="control-label">RT</label>
+                                        <select class="select form-control"></select>
+                                    </div>
+                                </div>
+                                <div class="col-sm-3" hidden>
+                                    <div class="form-group">
+                                        <label class="control-label">Nama</label>
+                                        <select class="select form-control"></select>
+                                    </div>
+                                </div>
+
                                 <div class="col-sm-3" >
                                     <div class="form-group">
                                         <label class="control-label">Usia Min.</label>
-                                        <input class="input form-control col-sm-4 text-left"></input>
+                                        <select class='select form-control excludedFromCustomFilter'>
+                                            <option></option>
+                                            @for($i = 16; $i < 30; $i++)
+                                            <option value="{{ $i }}"> {{$i}} </option>
+                                            @endfor()
+                                        </select>
+                                        {{-- <input class="input form-control col-sm-4 text-left"></input> --}}
                                     </div>
                                 </div>
                                 <div class="col-sm-3">
                                     <div class="form-group row">
                                         <label class="col-sm-4 ">Usia Maks.</label>
-                                        <input class="input form-control col-sm-4"></input>
+                                        <select class='select form-control excludedFromCustomFilter'>
+                                            <option></option>
+                                            @for($i = 16; $i < 30; $i++)
+                                            <option value="{{ $i }}"> {{$i}} </option>
+                                            @endfor()
+                                        </select>
                                     </div>
                                 </div>
+                                
                                 <div class="col-sm-3">
                                     <div class="form-group">
                                         <label class="control-label">Status Kawin</label>
                                         <select class="select form-control"></select>
                                     </div>
                                 </div>
-                                <div class="col-sm-3" hidden>
-                                    <div class="form-group">
-                                        <label class="control-label">No. RT</label>
-                                        <select class="select form-control"></select>
-                                    </div>
-                                </div>
                                
                                 <div class="col-sm-2 text-center">
-                                    <button class='btn btn-primary' style="font-size: 12px" onclick="cFilter()">Filter</button>
+                                    <button class='btn btn-primary' style="font-size: 12px" onclick="cFilterKarangtaruna()">Filter</button>
                                 </div> 
                             </div>
                         </div>
@@ -131,4 +156,156 @@
 
     </div><!-- .animated -->
 </div><!-- .content -->
+@endsection
+@section('customscript')
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        // tab = $('#bootstrap-data-table').DataTable()
+        // // tab.column(6).visible(false)
+        // tab.columns.adjust();
+        // // tab.column(0).search("^" + '[]' + "$", true, false).draw()
+        // // tab.column(5).search(false).draw()
+
+
+
+
+        // var DateFilterFunction = (function (oSettings, aData, iDataIndex) {
+        //     var dateStart = 7
+        //     var dateEnd = 8
+
+        //     console.log('Start')
+        //     console.log(aData)
+        //     var evalDate = aData[5]
+
+        //     if ((isNaN(dateStart) && isNaN(dateEnd)) ||
+        //         (isNaN(dateStart) && evalDate <= dateEnd) ||
+        //         (dateStart <= evalDate && isNaN(dateEnd)) ||
+        //         (dateStart <= evalDate && evalDate <= dateEnd)) {
+        //         return true;
+        //     }
+        //     return false;
+        // });
+
+
+        // start_date = 7
+        // end_date = 8
+        // $.fn.dataTableExt.afnFiltering.push(DateFilterFunction);
+        // tab.draw();
+        /* Custom filtering function which will search data in column four between two values */
+        $.fn.dataTable.ext.search.push(
+            function (settings, data, dataIndex) {
+                // var min = parseInt($('#min').val(), 10);
+                // var max = parseInt($('#max').val(), 10);
+                // var age = parseFloat(data[3]) || 0; // use data for the age column
+
+                var currentState = false
+                
+                var no_kk = $($('#cFilter').find('select')[0]).val()
+                var no_ktp = $($('#cFilter').find('select')[1]).val()
+                var no_rw = $($('#cFilter').find('select')[2]).val()
+                var no_rt = $($('#cFilter').find('select')[3]).val()
+                var nm_panggilan = $($('#cFilter').find('select')[4]).val()
+                var usia = $($('#cFilter').find('select')[5]).val()
+                var j_kelamin = $($('#cFilter').find('select')[6]).val()
+                var status_kawin = $($('#cFilter').find('select')[7]).val()
+
+                var min = $($('#cFilter').find('select')[5]).val()
+                var max = $($('#cFilter').find('select')[6]).val()
+
+                if(!isNotNull(min) && !isNotNull(max) && !isNotNull(status_kawin)){
+                    return true;
+                }
+
+                //not set
+                //not found
+                //founded
+                var gotusia = null
+                if(isNotNull(min) && isNotNull(max)){
+                    if(data[5] >= min && data[5] <= max){
+                        gotusia = true
+                    }else{
+                        gotusia = false
+                    }
+                }else if(isNotNull(min)){
+                    if(data[5] >= min){
+                        gotusia = true
+                    }else{
+                        gotusia = false
+                    }
+                }else if(isNotNull(max)){
+                    if(data[5] <= max){
+                        gotusia = true
+                    }else{
+                        gotusia = false
+                    }
+                }
+
+                var gotstatus_kawin = null
+                if(isNotNull(status_kawin)){
+                    if(data[7] === status_kawin){
+                        gotstatus_kawin = true
+                    }else{
+                        gotstatus_kawin = false
+                    }
+                }
+                // debugger
+                
+                if (gotusia === false || gotstatus_kawin === false){
+                    return false
+                } else if(gotusia && gotstatus_kawin === null){
+                    return gotusia
+                } else if(gotstatus_kawin && gotusia === null){
+                    return gotstatus_kawin
+                } else if(gotusia && gotstatus_kawin){
+                    return true
+                }else{
+                    return false
+                }
+            }
+        );
+
+        function isNotNull(input){
+            let result = true
+            if(input === ''){
+                result = false
+            }else if(input === undefined){
+                result = false
+            }else if(input === null){
+                result = false
+            }
+            return result
+        }
+
+
+
+        // $(document).ready(function () {
+        //     var table = $('#example').DataTable();
+
+        //     // Event listener to the two range filtering inputs to redraw on input
+        //     $('#min, #max').keyup(function () {
+        //         table.draw();
+        //     });
+        // });
+    })
+
+    function cFilterKarangtaruna() {
+        try {
+            // tab = $('#bootstrap-data-table').DataTable()
+
+            // $('#cFilter').find('select').each(function (i, e) {
+            //     if ($(e).val() !== '') {
+            //         tab.column(i).search("^" + $(e).val() + "$", true, false).draw()
+            //     } else {
+            //         tab.column(i).search($(e).val(), true, false).draw()
+            //     }
+            // })
+
+            tab = $('#bootstrap-data-table').DataTable()
+            tab.draw();
+        } catch (er) {
+
+        }
+    }
+
+</script>
 @endsection
